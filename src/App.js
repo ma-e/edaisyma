0464e898ce270ac267c4c6131e7214cb53214a1d
ma-e <ema@live.nmhu.edu>
2022-05-16 20:49:55 -0400

    Added music

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to my first gallery.</h1>
+        <h1>Daisy Ma.</h1>
+        <audio controls autoPlay>
+          <source src="http://streaming.tdiradio.com:8000/house.mp3" type="audio/mpeg" />
+        </audio>
       </header>
     </div>
   );
