0b054bc3230b33bc1da446cc59fbc7b4ab22b306
ma-e <ema@live.nmhu.edu>
2022-05-18 01:03:02 -0400

    Added git ignore file.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Menu from './Menu';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
         <audio controls autoPlay>
           <source src="http://streaming.tdiradio.com:8000/house.mp3" type="audio/mpeg" />
         </audio>
+        <p>Coming soon...</p>
+        {/* <Menu /> */}
       </header>
     </div>
   );
---
+++ src/Menu.js
@@ -0,0 +1,11 @@
+function Menu() {
+  return (
+    <div className="menu-item">
+        <span> + </span>
+        <a>Gallery</a>
+        <a>Contact</a>
+    </div>
+  );
+}
+
+export default Menu;
