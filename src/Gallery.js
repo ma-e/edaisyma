a1ddb6f4a12905f8cb77e3a5b45752fca64ad793
ma-e <39012403+ma-e@users.noreply.github.com>
2022-08-16 14:06:35 -0400

    Updated works.

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,6 +2,13 @@ import Slider from "react-slick";
 import mao from './config/works/mao.jpg';
 import babyBird from './config/works/babyBird.jpg';
 import cubanBird from './config/works/cubanBird.jpg';
+import pr1 from './config/works/br1.jpg';
+import pr0 from './config/works/pr0.jpg';
+import pr2 from './config/works/pr2.jpg';
+import pr3 from './config/works/pr3.jpg';
+import pr4 from './config/works/pr4.jpg';
+import pr5 from './config/works/pr5.jpg';
+
 
 // const images = [
 //     { url: "./config/works/cubanBird.jpg" },
@@ -9,7 +16,7 @@ import cubanBird from './config/works/cubanBird.jpg';
 //     { url: "./config/works/mao.jpg" },
 // ];
 
-const images = [mao, babyBird, cubanBird];
+const images = [pr0, pr1, pr2, pr3, pr4, pr5, mao, babyBird, cubanBird];
 
 function Gallery() {
     var settings = {
