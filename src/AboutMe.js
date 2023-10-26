6697b73047bfa46c796622e7162a402c3212a26c
ma-e <39012403+ma-e@users.noreply.github.com>
2023-10-25 23:34:21 -0700

    Upgrade about me page.

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -11,7 +11,7 @@ const AboutMe = () => {
         <div className="container">
             <div className="about-section">
                 <h1 className='title'>About Me</h1>
-                <p className=''>
+                <p>
                     I am a passionate and driven software engineer with a love for solving complex problems through code. With a strong foundation in computer science and a keen eye for detail, I thrive in the dynamic world of software development. My journey in this field has been characterized by a commitment to continuous learning and a desire to stay at the forefront of technology. Whether it's building web applications, crafting elegant algorithms, or optimizing databases, I enjoy every aspect of the software development process. I am dedicated to creating high-quality, efficient, and user-friendly solutions that make a positive impact. When I'm not coding, you can find me exploring new technologies, sharing knowledge with the developer community, or pursuing my other interests.
                 </p>
                 <p><button className="button"><a href="mailto:edaisyma@gmail.com">Contact</a></button></p>
