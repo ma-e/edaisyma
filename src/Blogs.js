da8593ac792c653cac3306b24bd068cbf63841ed
ma-e <39012403+ma-e@users.noreply.github.com>
2023-10-26 00:03:24 -0700

    Upgrade about me page.

--- src/Blogs.js
+++ src/Blogs.js
@@ -1,5 +1,5 @@
 const blogs = [
-    { id: 1, title: 'October 21, 2023', content: 'Launching shortly.' },
+    { id: 1, title: 'October 21, 2023', content: 'I can add blog with images here without touch source code is the goal.' },
     { id: 2, title: 'October 22, 2023', content: 'Launching shortly.' },
     // Add more blogs as needed
 ];
