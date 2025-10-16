3465aca6ae0d82b4027b135249b2a2fc56467665
ma-e <39012403+ma-e@users.noreply.github.com>
2025-10-15 22:21:17 -0700

    New full stack project

--- src/Blogs.js
+++
@@ -1,5 +0,0 @@
-const blogs = [
-    { id: 1, title: 'Do things with a heart', cover: require("./config/works/b1.png"), date: "2023-10-20", content: 'Launching shortly.' },
-    { id: 2, title: 'Nothing too much', cover: require("./config/works/b2.png"), date: "2023-10-28", content: 'Launching shortly.' },
-];
-export default blogs;
