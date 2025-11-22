4c24d97ab99acda6590c3f866357f9227dae625f
ma-e <39012403+ma-e@users.noreply.github.com>
2025-11-22 14:17:25 -0800

    test new proj

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { AdminProvider } from './context/AdminContext'; // We'll create this
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AdminProvider>
-      <App />
-    </AdminProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- frontend/src/pages/Comment/Comment.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Comment: React.FC = () => {
-  return <h1>Comment Page</h1>;
-};
-
-export default Comment;
