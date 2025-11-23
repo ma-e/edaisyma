dcc194f25cd3a33fca7e794d7e43a0c2cb9cf653
ma-e <39012403+ma-e@users.noreply.github.com>
2025-11-23 13:58:00 -0800

    updated front url

--- frontend/src/pages/Comment/Comment.tsx
+++ frontend/src/pages/Comment/Comment.tsx
@@ -20,7 +20,7 @@ const Comment: React.FC = () => {
 
   const fetchComments = async () => {
     try {
-      const res = await fetch('http://127.0.0.1:8000/comments/');
+      const res = await fetch('https://edaisyma.onrender.com/comments/');
       if (!res.ok) throw new Error('Failed to fetch comments');
       const data: Comment[] = await res.json();
       setComments(data);
@@ -38,7 +38,7 @@ const Comment: React.FC = () => {
     setSubmitting(true);
 
     try {
-      const res = await fetch('http://127.0.0.1:8000/comments/', {
+      const res = await fetch('https://edaisyma.onrender.com/comments/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ id: 0, name, comment: text }),
