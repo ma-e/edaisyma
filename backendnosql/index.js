153562028507a74f6885389ca0d09b3056d83454
ma-e <39012403+ma-e@users.noreply.github.com>
2026-01-10 15:22:24 -0800

    Ready for Render deployment

--- backendnosql/index.js
+++ backendnosql/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json()); // <-- required to parse JSON
 
 // Global logging
@@ -59,7 +59,7 @@ app.get("/wishlist", async (req, res) => {
 });
 
 // Bind to IPv4
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
